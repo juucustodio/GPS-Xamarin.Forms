4d5477e450bc68c86e080e27bcfe5d6cb267b157
Juliano Custódio <juliano.custodio@hotmail.com.br>
2017-07-25 14:56:20 -0300

    Implements get location

--- DemoGPS/DemoGPS/DemoGPS/MainPage.xaml.cs
+++ DemoGPS/DemoGPS/DemoGPS/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using Plugin.Geolocator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,17 @@ namespace DemoGPS
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void GetPosition(object sender, EventArgs e)
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 50;
+
+            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
+
+            LongitudeLabel.Text = string.Format("{0:0.0000000}", position.Longitude); 
+            LatitudeLabel.Text = string.Format("{0:0.0000000}", position.Latitude); 
         }
     }
 }
